be4daffca95eb2e14004cffdf6d8e4dfe07b2b0e
micahbohemian <Micahjohnson1983@gmail.com>
2016-03-30 14:27:52 -0500

    javavscript for jquerry exercise.  addind id selectors 4.11

---
+++ public/js/jquery_exercises.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    alert( 'The page has finished loading!' );
+}
+
+$(document).ready(function(){
+    var sayHello = $("#latin-crap").html();
+    alert(sayHello);
+
+});
+// $("codeup");
+
+// .instructors

fd151a4d47e109c5df68f859fd8757ea63716ab7
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-17 09:08:46 -0500

    9.1.4 code cleaned up.  form moved to seperate file to add new national park.  notes need amended but code is much clearer

--- public/national_parks.php
+++ public/national_parks.php
@@ -5,18 +5,23 @@ require ('../db_connect.php');
 require_once ('../Input.php');
 
 
-
-function pageController($dbc)
-{
-    // setting page limit variable for number of parks shown per page
-    $pageLimit = 4;
-    // setting the count.  if it has a count use that count, if not set it to 0
-    $count = !(Input::has('count')) ? 0 : Input::get('count');
+function maxCounter($dbc,$pageLimit){
+    // doing a query to find out the number of parks in the database
+    $maxCount = 'SELECT count(*) FROM national_parks';
+    $maxCount = $dbc->query($maxCount);
+    $maxCount = $maxCount->fetchColumn();
+    // divide that count by the pagelimit(4)
+    $maxCount = $maxCount / $pageLimit;
+    // round it so there isnt a decimal place in the number of total pages.
+    // maxCount is used later in the next/previous buttons to tell if there is another
+    // page or not.
+    $maxCount = round($maxCount);
+    
+    return $maxCount;
+}
+function paginate($count,$dbc,$pageLimit){
     // establishing where to start on new pages.  take the count and multiply it by 4 to set the offset
     $offsetNumber = $count * $pageLimit; 
-
-    // the array of parks
-    $data = [];
     // this design makes it possible for user to input malicious code..
     // $stmt = $dbc->prepare("SELECT * FROM national_parks LIMIT {$pageLimit} OFFSET {$offsetNumber}");
     // this design keeps users from inputing malicious code into variables..
@@ -28,44 +33,27 @@ function pageController($dbc)
     $stmt->bindValue(':offsetNumber',  $offsetNumber,  PDO::PARAM_INT);
 
     $stmt->execute();
+    return $stmt->fetchALL(PDO::FETCH_ASSOC);
+}
+
+function pageController($dbc)
+{
+
+    // setting page limit variable for number of parks shown per page
+    $pageLimit = 4;
+    // the array of parks
+    $data = [];
+    // setting the count.  if it has a count use that count, if not set it to 0
+    $count = !(Input::has('count')) ? 0 : Input::get('count');
 
     // go to the database and fetch all the parks and count
     // set them equal to  a variable for usage below
-    $data['parks'] = $stmt->fetchALL(PDO::FETCH_ASSOC);
+    $data['parks'] = paginate($count, $dbc, $pageLimit);
     $data['count'] = $count;
     
-    // doing a query to find out the number of parks in the database
-    $maxCount = 'SELECT count(*) FROM national_parks';
-    $maxCount = $dbc->query($maxCount);
-    $maxCount = $maxCount->fetchColumn();
-    // divide that count by the pagelimit(4)
-    $maxCount = $maxCount / $pageLimit;
-    // round it so there isnt a decimal place in the number of total pages.
-    // maxCount is used later in the next/previous buttons to tell if there is another
-    // page or not.
-    $maxCount = round($maxCount);
-
-
     // send the max to the page so we can use it
-    $data['maxCount'] = $maxCount;
+    $data['maxCount'] = maxCounter($dbc,$pageLimit);
     
-    // checking to make sure the form has every field filled out.  if it doesnt it should reload form
-    // and alert user of fail.  it however errors out.  it does warn them but forces you to reload the page
-    if (Input::has('name') && Input::has('location') && Input::has('date_established') && Input::has('area_in_acres') && Input::has('animal_poop_prevalence') && Input::has('volcano_danger') ) {
-           
-            // preparing the data from the form to be binded to the proper fields.
-            $stmt = $dbc->prepare('INSERT INTO national_parks (name, location, date_established, area_in_acres, animal_poop_prevalence, volcano_danger) VALUES (:name, :location, :date_established, :area_in_acres, :animal_poop_prevalence, :volcano_danger )');
-            $stmt->bindValue(':name',  Input::get('name'),  PDO::PARAM_STR);
-            $stmt->bindValue(':location',  Input::get('location'),  PDO::PARAM_STR);
-            $stmt->bindValue(':date_established',  Input::get('date_established'),  PDO::PARAM_STR);
-            $stmt->bindValue(':area_in_acres',  Input::get('area_in_acres'),  PDO::PARAM_INT);
-            $stmt->bindValue(':animal_poop_prevalence',  Input::get('animal_poop_prevalence'),  PDO::PARAM_STR);
-            $stmt->bindValue(':volcano_danger',  Input::get('volcano_danger'),  PDO::PARAM_STR);
-
-            $stmt->execute();
-    }
-
-
     return $data;
 }
 extract(pageController($dbc));
@@ -101,53 +89,7 @@ maxcount -1 -->
     <a href="national_parks.php?count=<?= $count + 1 ?>">Next</a>
 <?php } ?>
 
-<!-- form for submitting a new park -->
-<h2>Add a National Park</h2>
-<?php 
-    // this submit button will only work if all fields "has" info in it
-    if (Input::has('submit')) 
-    {
-        // if every field is filled in sumbit button will work
-        if (
-            (Input::has('name') && (Input::get('name')!="")) &&
-            (Input::has('location') && (Input::get('location')!="")) &&
-            (Input::has('date_established') && (Input::get('date_established')!="")) &&
-            (Input::has('area_in_acres') && (Input::get('area_in_acres')!="")) &&
-            (Input::has('animal_poop_prevalence') && (Input::get('animal_poop_prevalence')!="")) &&
-            (Input::has('volcanic_danger') && (Input::get('volcanic_danger')!=""))
-            )
-        {
-            echo "Database Updated!";
-        // if fields are left empty give this error message.  but also error like crazy
-        } else {
-            echo "Please Fill Out the Form Completely!";
-        }
-    }
-?>
-    <!-- where the form is posting to.. (itself) -->
-    <form method="POST" action="/national_parks.php">
-        <p>
-            <input type="text" name="name" placeholder="Park Name: ">
-        </p>
-        <p>
-            <input type="text" name="location" placeholder="Location: ">
-        </p>
-        <p>
-            <!-- type is date so that is reformats the same as the datatype set in mysql migration file -->
-            <input type="date" name="date_established" placeholder="Date: YYYY-MM-DD ">
-        </p>
-        <p>
-            <input type="text" name="area_in_acres" placeholder="Area In Acres: ">
-        </p>
-        <p>
-            <input type="text" name="animal_poop_prevalence" placeholder="Animal Poop Characteristics: ">
-        </p>
-        <p>
-            <input type="text" name="volcano_danger" placeholder="Volcano Danger: ">
-        </p>
-        
-        <button type="Submit">Submit</button>
-    </form>
+<a href="new_national_parks.php">Add a new park!</a>
 
 
 </body>
